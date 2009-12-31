e34ccb7c36676727d0a713b6e83769990ba90961
Carl Hicks <carl.hicks@gmail.com>
2009-12-30 18:34:39 -0800

    Better parsing and error handling for google_maps module Corrected some invalid employee associations

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,7 @@
 class EmployeesController < ApplicationController
   
   def index
-    @employees = Employee.find(:all, :conditions => {:status => "Active"})
+    @employees = Employee.find(:all, :include => [:department, :location], :conditions => {:status => "Active"})
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @employees }
--- lib/google_maps.rb
+++ lib/google_maps.rb
@@ -9,7 +9,13 @@ require 'json'
 KEY   = "ABQIAAAA33LdtDapxLpK1y-7KvqQAhSpbr_YJ8LlxhUjJExVrrRs8W66CxR9-FAf750PJ1utZfR_UvWXQuMkrA"
 MIN_ACCURACY = 6
 
-class AccuracyException < Exception
+class AccuracyException < RuntimeError
+end
+
+class ParsingException < RuntimeError
+end
+
+class RequestException < RuntimeError
 end
 
 # This method runs the FIRST address through the google maps object
@@ -22,18 +28,30 @@ def self.lookup(addr)
   query         = "http://maps.google.com/maps/geo?q=" << URI.escape(address) <<  "&output=json&sensor=false&key=" << KEY
   response      = Net::HTTP.get_response(URI.parse(query))
   json          = JSON.parse(response.body)
-  case json["Status"]["code"]
-    when "200" then json
-    when "500" then raise "G_GEO_SERVER_ERROR"
-    when "601" then raise "G_GEO_MISSING_QUERY"
-    when "602" then raise "G_GEO_UNKNOWN_ADDRESS"
-    when "603" then raise "G_GEO_UNAVAILABLE_ADDRESS"
-    when "610" then raise "G_GEO_BAD_KEY"
-    when "620" then raise "G_GEO_TOO_MANY_QUERIES"
+  status        = json["Status"]["code"]
+  case status
+    when 200 then json
+    when 400 then raise RequestException, "Request could not be parsed: #{query}"
+    when 500 then raise RequestException, "Server Error"
+    when 601 then raise RequestException, "Empty/Incomplete Query #{query}"
+    when 602 then raise RequestException, "Unknown Location: #{addr}"
+    when 603 then raise RequestException, "Unavailable Address: #{addr}"
+    when 610 then raise RequestException, "Invalid API Key: #{KEY}"
+    when 620 then raise RequestException, "Request Limit Exceeded"
+    else raise RequestException, "Unknown Status: #{status}"
   end
 
+  pp json
   accuracy = json["Placemark"][0]["AddressDetails"]["Accuracy"].to_i
   raise AccuracyException, "Accuracy of #{accuracy} below accepted minimum" if accuracy < MIN_ACCURACY
+
+  street      = "Unknown"
+  city        = "Unknown"
+  state       = "Unknown"
+  postal_code = "Unknown"
+  country     = "Unknown"
+  coords      = "0,0,0"
+  address     = "Unknown"
 
   coords      = json["Placemark"][0]["Point"]["coordinates"].join ","
   name        = json["name"]
@@ -43,14 +61,45 @@ def self.lookup(addr)
   location    = json["Placemark"][0]["AddressDetails"]["Country"] 
 
   country     = placemark["CountryNameCode"]
-  state       = location["AdministrativeArea"]["AdministrativeAreaName"]
-  locality    = location["AdministrativeArea"]["SubAdministrativeArea"]["Locality"] 
-  if locality["DependentLocality"]
-    locality  = location["AdministrativeArea"]["SubAdministrativeArea"]["Locality"]["DependentLocality"]
+
+  area        = {}
+  # This generally contains the state or region. 
+  if location["AdministrativeArea"]
+    area      = location["AdministrativeArea"]
+    state     = area["AdministrativeAreaName"]
+  # This is used for small countries that dont have states
+  elsif location["SubAdministrativeArea"]
+    area      = location["SubAdministrativeArea"]
+    state     = area["SubAdministrativeAreaName"]
+  else 
+    raise ParseException, "Couldn't determine state/region for #{addr}"
   end
-  city        = locality["LocalityName"]
-  postal_code = locality["PostalCode"]["PostalCodeNumber"]
-  street      = locality["Thoroughfare"]["ThoroughfareName"]
+
+  locality    = {}
+  # Locality == City 
+  if area["Locality"]
+    locality    = area["Locality"]
+  # We are part of a region or county.  
+  elsif area["SubAdministrativeArea"]["Locality"]
+    locality    = area["SubAdministrativeArea"]["Locality"]
+    if area["SubAdministrativeArea"]["Locality"]["DependentLocality"]
+      locality  = area["SubAdministrativeArea"]["Locality"]["DependentLocality"]
+    end
+  else
+    raise ParseException, "Couldn't determine city/locale for #{addr}"
+  end
+
+  if locality["LocalityName"]
+    city      = locality["LocalityName"]
+  end
+
+  if locality["PostalCode"]
+    postal_code = locality["PostalCode"]["PostalCodeNumber"]
+  end
+
+  if locality["Thoroughfare"]
+    street      = locality["Thoroughfare"]["ThoroughfareName"] 
+  end
   
   return      {:street_address => street, :city => city, :state => state, :postal_code => postal_code, :country => country, :coordinates => coords, :name => name, :address => address}
 
